ffb90bb0b7e357f5cbbabb907da18d5f906a6c3d
anoop7897 <82763342+anoop7897@users.noreply.github.com>
2024-01-10 04:39:26 +0000

    strings

---
+++ index2.js
@@ -0,0 +1,13 @@
+let name = "Anoop Maurya";
+console.log(`My name is ${name}`);
+console.log(name.length);
+console.log(name[2]);
+console.log(name[name.length-1]);
+let className = 'btn';
+className += 'btn-primary';
+className += 'none';
+console.log(className);
+let num = 23;
+let strNum = String(num);
+console.log(typeof strNum);
+console.log(num.toString());
